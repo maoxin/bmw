6d4ddb199ecf0e711fa0fa70b5b20d4be86b6bd0
maoxin <maoxin.horizon@gmail.com>
2018-04-04 12:49:27 +0800

    add pad to Convnet, and fine tune the crop algorithm
    * use pad=1 in Conv2d to ensure the Convnet do not change the size of the input
    * use new crop method to avoid [0:-0]
    * crop is still needed when pad is added, because the maxpooling with 'floor' mod will cause size reduction

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -24,14 +24,24 @@ class UNet(nn.Module):
 
     def forward(self, x):
         x1 = self.inc(x)
+        print(f'x1: {x1.shape}')
         x2 = self.down1(x1)
+        print(f'x2: {x2.shape}')
         x3 = self.down2(x2)
+        print(f'x3: {x3.shape}')
         x4 = self.down3(x3)
+        print(f'x4: {x4.shape}')
         x5 = self.down4(x4)
+        print(f'x5: {x5.shape}')
         x = self.up1(x5, x4)
+        print(f'up1: {x.shape}')
         x = self.up2(x, x3)
+        print(f'up2: {x.shape}')
         x = self.up3(x, x2)
+        print(f'up3: {x.shape}')
         x = self.up4(x, x1)
+        print(f'up4: {x.shape}')
         x = self.outc(x)
+        print(f'out: {x.shape}')
         
         return x
